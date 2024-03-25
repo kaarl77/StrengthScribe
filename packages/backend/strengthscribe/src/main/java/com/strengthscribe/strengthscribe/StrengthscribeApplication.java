38668c4ef68b1e5831af67efb7acdc399e470db9
Alex <alexpicior@yahoo.com>
2024-03-25 21:23:09 +0200

    .

---
+++ packages/backend/strengthscribe/src/main/java/com/strengthscribe/strengthscribe/StrengthscribeApplication.java
@@ -0,0 +1,13 @@
+package com.strengthscribe.strengthscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrengthscribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrengthscribeApplication.class, args);
+	}
+
+}

3fb283c43968c9dec0589131290774c5541fc2dc
Alex <alexpicior@yahoo.com>
2024-05-03 13:17:43 +0300

    Added crud for workout and added swagger

--- packages/backend/strengthscribe/src/main/java/com/strengthscribe/strengthscribe/entity/Workout.java
+++ packages/backend/strengthscribe/src/main/java/com/strengthscribe/strengthscribe/entity/Workout.java
@@ -3,24 +3,18 @@ package com.strengthscribe.strengthscribe.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Entity
 @Data
-public class Record {
+@Entity
+public class Workout {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
     @Column
-    private Integer set;
-
-    @Column
-    private Integer repetitions;
-
-    @Column
-    private Float weight;
+    private String name;
 
     @ManyToOne
-    @JoinColumn(name = "exercise_id")
-    private Exercise exercise;
+    @JoinColumn(name = "user_id")
+    private User user;
 }
--- packages/backend/strengthscribe/src/main/java/com/strengthscribe/strengthscribe/repository/ExerciseRepository.java
+++ packages/backend/strengthscribe/src/main/java/com/strengthscribe/strengthscribe/repository/ExerciseRepository.java
@@ -2,6 +2,7 @@ package com.strengthscribe.strengthscribe.repository;
 
 import com.strengthscribe.strengthscribe.entity.Exercise;
 import com.strengthscribe.strengthscribe.entity.User;
+import com.strengthscribe.strengthscribe.entity.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,6 +11,8 @@ public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     List<Exercise> findByNameContainingAndUser(String name, User user);
 
     List<Exercise> findByUser(User user);
+
+    List<Exercise> findByWorkout(Workout workout);
 
     Exercise save(Exercise exercise);
 
